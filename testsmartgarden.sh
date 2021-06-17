ee3b0395581daa3643732ad87f1a2a661b172f08
Allan Mølgaard <am@amd.dk>
2021-06-17 08:44:55 +0200

    Update to Smartgarden and Windows

--- testsmartgarden.sh
+++ testsmartgarden.sh
@@ -2,14 +2,6 @@
 #
 # Test the JMeter Docker image using a trivial test plan.
 
-# Example for using User Defined Variables with JMeter
-# These will be substituted in JMX test script
-# See also: http://stackoverflow.com/questions/14317715/jmeter-changing-user-defined-variables-from-command-line
-export TARGET_HOST="www.map5.nl"
-export TARGET_PORT="80"
-export TARGET_PATH="/kaarten.html"
-export TARGET_KEYWORD="Kaartdiensten"
-
 T_DIR=tests/smartgarden
 
 # Reporting dir: start fresh
@@ -20,8 +12,6 @@ mkdir -p ${R_DIR}
 /bin/rm -f ${T_DIR}/test-plan.jtl ${T_DIR}/jmeter.log  > /dev/null 2>&1
 
 ./run.sh -Dlog_level.jmeter=DEBUG \
-	-JTARGET_HOST=${TARGET_HOST} -JTARGET_PORT=${TARGET_PORT} \
-	-JTARGET_PATH=${TARGET_PATH} -JTARGET_KEYWORD=${TARGET_KEYWORD} \
 	-n -t ${T_DIR}/test-plan.jmx -l ${T_DIR}/test-plan.jtl -j ${T_DIR}/jmeter.log \
 	-e -o ${R_DIR}
 
